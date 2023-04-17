13b60dfb8cbc9636a4ebab32b8b7ced215806b3a
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-04-17 19:51:55 +0700

    Add splash screen and make some setup for it

---
+++ app/src/main/java/com/listgithubusersinglescreen/ui/splash/SplashFragment.kt
@@ -0,0 +1,42 @@
+package com.listgithubusersinglescreen.ui.splash
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.listgithubusersinglescreen.R
+import com.listgithubusersinglescreen.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.let {
+            it.postDelayed({
+                view.findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            }, 500)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
+    }
+}

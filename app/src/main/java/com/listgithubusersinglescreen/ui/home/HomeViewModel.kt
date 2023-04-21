1915ad1e026ae2c9a2ab63430c28ebe1ba7c3d51
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-04-21 09:15:17 +0700

    initial commit in Rombak_1 branch

--- app/src/main/java/com/listgithubusersinglescreen/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/listgithubusersinglescreen/ui/home/HomeViewModel.kt
@@ -9,12 +9,23 @@ class HomeViewModel(
     private val userRepository: Repository
 ) : ViewModel() {
 
+    val searchQuery = MutableLiveData<String>()
+
+    /*private val _searchViewExpanded = MutableLiveData(false)
     private val _searchText = MutableLiveData<String>()
+
+    val searchViewExpanded: LiveData<Boolean>
+        get() = _searchViewExpanded
+
+    fun setSearchExpand(visible: Boolean){
+        _searchViewExpanded.value = visible
+    }
+
     val searchText: LiveData<String>
         get() = _searchText
     fun setSearchText(text: String) {
         _searchText.value = text
-    }
+    }*/
     fun getFreshUser() = userRepository.getUsers(null)
     fun getSearchUser(login: String) = userRepository.getUsers(login)
 }

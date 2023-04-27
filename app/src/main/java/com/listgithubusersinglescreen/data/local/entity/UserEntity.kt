1eb4cd12c2e581f2b63ccbd02a1c5055bbb07db9
Jonry Simbolon <jonrysimbolon19@gmail.com>
2023-04-27 10:55:07 +0700

    Configure code of room's database and table so ease to maintain

--- app/src/main/java/com/listgithubusersinglescreen/data/local/entity/UserEntity.kt
+++ app/src/main/java/com/listgithubusersinglescreen/data/local/entity/UserEntity.kt
@@ -7,28 +7,55 @@ import com.listgithubusersinglescreen.BuildConfig
 
 @Entity(tableName = BuildConfig.USER_TBL_NEW)
 class UserEntity(
-    @field:ColumnInfo(name = "nodeId")
+    @field:ColumnInfo(name = ComponentUser.nodeId)
     @field:PrimaryKey
     val nodeId: String,
 
-    @field:ColumnInfo(name = "login")
+    @field:ColumnInfo(name = ComponentUser.login)
     val login: String,
 
-    @field:ColumnInfo(name = "followers")
+    @field:ColumnInfo(name = ComponentUser.followers)
     val followers: Int,
 
-    @field:ColumnInfo(name = "avatar_url")
+    @field:ColumnInfo(name = ComponentUser.avatar_url)
     val avatarUrl: String,
 
-    @field:ColumnInfo(name = "following")
+    @field:ColumnInfo(name = ComponentUser.following)
     val following: Int,
 
-    @field:ColumnInfo(name = "name")
+    @field:ColumnInfo(name = ComponentUser.name)
     val name: String,
 
-    @field:ColumnInfo(name = "url")
+    @field:ColumnInfo(name = ComponentUser.url)
     val url: String,
 
-    @field:ColumnInfo(name = "loved")
+    @field:ColumnInfo(name = ComponentUser.loved)
     var isLoved: Boolean = false
 )
+
+/**
+ *
+ * name of table set on build.gradle
+ *
+ *  version  |         name        |
+ *    1      |       user_tbl      | id | nodeId_user | nodeId | login | followers | avatar_url | following | name | url | follow_status |
+ *    2      |      user_01_tbl    | id | nodeId_user | nodeId | login | followers | avatar_url | following | name | url | follow_status |
+ *
+ *
+ *
+ *
+ *
+ */
+
+class ComponentUser{
+    companion object {
+        const val nodeId: String = "nodeId"
+        const val login: String = "login"
+        const val followers: String = "followers"
+        const val avatar_url: String = "avatar_url"
+        const val following: String = "following"
+        const val name: String = "name"
+        const val url: String = "url"
+        const val loved: String = "loved"
+    }
+}
--- app/src/main/java/com/listgithubusersinglescreen/data/local/room/db/GithubListUserDatabase.kt
+++ app/src/main/java/com/listgithubusersinglescreen/data/local/room/db/GithubListUserDatabase.kt
@@ -4,14 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import com.listgithubusersinglescreen.BuildConfig
 import com.listgithubusersinglescreen.data.local.entity.FollowEntity
 import com.listgithubusersinglescreen.data.local.entity.UserEntity
 import com.listgithubusersinglescreen.data.local.room.FollowDao
 import com.listgithubusersinglescreen.data.local.room.UserDao
-
-const val oldVersion = 1
-const val currentVersion = 2
 
 @Database(
     entities = [UserEntity::class, FollowEntity::class],
@@ -25,57 +21,26 @@ abstract class GithubListUserDatabase : RoomDatabase() {
 
 }
 
-
-// boolean in room == integer in sqlite
-private val CREATE_USER_TBL = """
-    CREATE TABLE IF NOT EXISTS ${BuildConfig.USER_TBL_NEW} (
-        nodeId TEXT PRIMARY KEY NOT NULL,
-        login TEXT NOT NULL,
-        followers INTEGER NOT NULL,
-        avatar_url TEXT NOT NULL,
-        following INTEGER NOT NULL,
-        name TEXT NOT NULL,
-        url TEXT NOT NULL,
-        loved INTEGER NOT NULL
-        )
-""".trimIndent()
-
-private val CREATE_FOLLOW_TBL = """
-    CREATE TABLE IF NOT EXISTS ${BuildConfig.FOLLOW_TBL_NEW} (
-        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-        nodeId_user TEXT NOT NULL, 
-        nodeId TEXT NOT NULL, 
-        login TEXT NOT NULL, 
-        followers INTEGER NOT NULL, 
-        avatar_url TEXT NOT NULL, 
-        following INTEGER NOT NULL, 
-        name TEXT NOT NULL, 
-        url TEXT NOT NULL, 
-        follow_status TEXT NOT NULL,
-        FOREIGN KEY(nodeId_user) REFERENCES ${BuildConfig.USER_TBL_NEW}(nodeId) ON DELETE CASCADE
-        )
-""".trimIndent()
-
-private val INSERT_OLD_DATA_TO_NEW_USER_TBL = """
-    INSERT INTO ${BuildConfig.USER_TBL_NEW} (nodeId, login, followers, avatar_url, following, name, url, loved)
-    SELECT nodeId, login, followers, avatar_url, following, name, url, loved FROM user_tbl
-""".trimIndent()
-
-private val DROP_TABLE_USER_IF_EXIST = """
-    DROP TABLE IF EXISTS ${BuildConfig.USER_TBL_OLD}
-""".trimIndent()
-
-
 val migration = object : Migration(oldVersion, currentVersion) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        // Create a new table with the desired schema
+
+        /** (1) CREATE A NEW TABLE WITH THE DESIRED SCHEMA
+         *      :.  Create user tbl
+         *      :.  create follow tbl
+         */
         db.execSQL(CREATE_USER_TBL)
         db.execSQL(CREATE_FOLLOW_TBL)
 
-        // Copy data from old table to new table
+
+        /** (2) COPY DATA FROM OLD TABLE TO NEW TABLE
+         *      :.  Copy data from old user tbl to new user tbl
+         */
         db.execSQL(INSERT_OLD_DATA_TO_NEW_USER_TBL)
 
-        // Drop the old table
+
+        /** (3) DROP THE OLD TABLE
+         *      :.  Drop old user tbl if exist
+         */
         db.execSQL(DROP_TABLE_USER_IF_EXIST)
     }
 }
